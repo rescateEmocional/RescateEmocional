33174644923b49b564408266843001ee04c5b9ce
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-03-26 20:52:17 -0600

    se crearon los controladores y vistas de -tabla de roles -tabla de usuarios -tabla de organizaciones -tabla de administradores -tabla de conversaciones

---
+++ RescateEmocional/Controllers/ConversacionController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RescateEmocional.Models;
+
+namespace RescateEmocional.Controllers
+{
+    public class ConversacionController : Controller
+    {
+        private readonly RescateEmocionalContext _context;
+
+        public ConversacionController(RescateEmocionalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Conversacion
+        public async Task<IActionResult> Index()
+        {
+            var rescateEmocionalContext = _context.Conversacions.Include(c => c.IdorganizacionNavigation).Include(c => c.IdusuarioNavigation);
+            return View(await rescateEmocionalContext.ToListAsync());
+        }
+
+        // GET: Conversacion/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conversacion = await _context.Conversacions
+                .Include(c => c.IdorganizacionNavigation)
+                .Include(c => c.IdusuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Idconversacion == id);
+            if (conversacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(conversacion);
+        }
+
+        // GET: Conversacion/Create
+        public IActionResult Create()
+        {
+            ViewData["Idorganizacion"] = new SelectList(_context.Organizacions, "Idorganizacion", "Idorganizacion");
+            ViewData["Idusuario"] = new SelectList(_context.Usuarios, "Idusuario", "Idusuario");
+            return View();
+        }
+
+        // POST: Conversacion/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Idconversacion,Idusuario,Idorganizacion,FechaInicio,Mensaje")] Conversacion conversacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(conversacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Idorganizacion"] = new SelectList(_context.Organizacions, "Idorganizacion", "Idorganizacion", conversacion.Idorganizacion);
+            ViewData["Idusuario"] = new SelectList(_context.Usuarios, "Idusuario", "Idusuario", conversacion.Idusuario);
+            return View(conversacion);
+        }
+
+        // GET: Conversacion/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conversacion = await _context.Conversacions.FindAsync(id);
+            if (conversacion == null)
+            {
+                return NotFound();
+            }
+            ViewData["Idorganizacion"] = new SelectList(_context.Organizacions, "Idorganizacion", "Idorganizacion", conversacion.Idorganizacion);
+            ViewData["Idusuario"] = new SelectList(_context.Usuarios, "Idusuario", "Idusuario", conversacion.Idusuario);
+            return View(conversacion);
+        }
+
+        // POST: Conversacion/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idconversacion,Idusuario,Idorganizacion,FechaInicio,Mensaje")] Conversacion conversacion)
+        {
+            if (id != conversacion.Idconversacion)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(conversacion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConversacionExists(conversacion.Idconversacion))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Idorganizacion"] = new SelectList(_context.Organizacions, "Idorganizacion", "Idorganizacion", conversacion.Idorganizacion);
+            ViewData["Idusuario"] = new SelectList(_context.Usuarios, "Idusuario", "Idusuario", conversacion.Idusuario);
+            return View(conversacion);
+        }
+
+        // GET: Conversacion/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conversacion = await _context.Conversacions
+                .Include(c => c.IdorganizacionNavigation)
+                .Include(c => c.IdusuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Idconversacion == id);
+            if (conversacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(conversacion);
+        }
+
+        // POST: Conversacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var conversacion = await _context.Conversacions.FindAsync(id);
+            if (conversacion != null)
+            {
+                _context.Conversacions.Remove(conversacion);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConversacionExists(int id)
+        {
+            return _context.Conversacions.Any(e => e.Idconversacion == id);
+        }
+    }
+}
---
+++ RescateEmocional/Controllers/OrganizacionController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RescateEmocional.Models;
+
+namespace RescateEmocional.Controllers
+{
+    public class OrganizacionController : Controller
+    {
+        private readonly RescateEmocionalContext _context;
+
+        public OrganizacionController(RescateEmocionalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Organizacion
+        public async Task<IActionResult> Index()
+        {
+            var rescateEmocionalContext = _context.Organizacions.Include(o => o.IdrolNavigation);
+            return View(await rescateEmocionalContext.ToListAsync());
+        }
+
+        // GET: Organizacion/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organizacion = await _context.Organizacions
+                .Include(o => o.IdrolNavigation)
+                .FirstOrDefaultAsync(m => m.Idorganizacion == id);
+            if (organizacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(organizacion);
+        }
+
+        // GET: Organizacion/Create
+        public IActionResult Create()
+        {
+            ViewData["Idrol"] = new SelectList(_context.Rols, "Idrol", "Idrol");
+            return View();
+        }
+
+        // POST: Organizacion/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Idorganizacion,Nombre,Descripcion,Horario,Ubicacion,Estado,Idrol")] Organizacion organizacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(organizacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Idrol"] = new SelectList(_context.Rols, "Idrol", "Idrol", organizacion.Idrol);
+            return View(organizacion);
+        }
+
+        // GET: Organizacion/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organizacion = await _context.Organizacions.FindAsync(id);
+            if (organizacion == null)
+            {
+                return NotFound();
+            }
+            ViewData["Idrol"] = new SelectList(_context.Rols, "Idrol", "Idrol", organizacion.Idrol);
+            return View(organizacion);
+        }
+
+        // POST: Organizacion/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idorganizacion,Nombre,Descripcion,Horario,Ubicacion,Estado,Idrol")] Organizacion organizacion)
+        {
+            if (id != organizacion.Idorganizacion)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(organizacion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrganizacionExists(organizacion.Idorganizacion))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Idrol"] = new SelectList(_context.Rols, "Idrol", "Idrol", organizacion.Idrol);
+            return View(organizacion);
+        }
+
+        // GET: Organizacion/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organizacion = await _context.Organizacions
+                .Include(o => o.IdrolNavigation)
+                .FirstOrDefaultAsync(m => m.Idorganizacion == id);
+            if (organizacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(organizacion);
+        }
+
+        // POST: Organizacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var organizacion = await _context.Organizacions.FindAsync(id);
+            if (organizacion != null)
+            {
+                _context.Organizacions.Remove(organizacion);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrganizacionExists(int id)
+        {
+            return _context.Organizacions.Any(e => e.Idorganizacion == id);
+        }
+    }
+}
