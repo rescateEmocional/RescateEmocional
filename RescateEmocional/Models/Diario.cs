0126b6f19cf1c1b2dbdc66cc7b8eb583ff36b65b
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-04-02 11:52:02 -0600

    se hicieron validaciones para el model de diario

--- RescateEmocional/Models/Diario.cs
+++ RescateEmocional/Models/Diario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RescateEmocional.Models;
 
@@ -9,10 +10,15 @@ public partial class Diario
 
     public int Idusuario { get; set; }
 
+    [Required(ErrorMessage = "El titulo es obligatorio.")]
+
     public string Titulo { get; set; } = null!;
 
+    [MaxLength(2000)]
+    [Required(ErrorMessage = "El contenido es obligatorio.")]
     public string Contenido { get; set; } = null!;
 
+    [Required(ErrorMessage = "La fecha es obligatorio.")]
     public DateTime FechaCreacion { get; set; }
 
     public virtual Usuario? IdusuarioNavigation { get; set; } = null!;
