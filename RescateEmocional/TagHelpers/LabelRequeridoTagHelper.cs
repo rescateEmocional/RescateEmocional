c5870860bbf3a0d9694c65af5136761d27350cb5
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-04-04 10:36:30 -0600

    solo el admin puede acceder a Rol, se creo taghelper que ayuda a validar los campos obligatorios poniendole *

---
+++ RescateEmocional/TagHelpers/LabelRequeridoTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace RescateEmocional.TagHelpers
+{
+    [HtmlTargetElement("label", Attributes = "asp-for")]
+    public class LabelRequeridoTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression AspFor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // Obtiene el nombre del campo
+            var labelText = AspFor.Metadata.DisplayName ?? AspFor.Name;
+
+            // Verifica si tiene el atributo Required
+            var isRequired = AspFor.Metadata.ValidatorMetadata
+                .OfType<RequiredAttribute>()
+                .Any();
+
+            // Genera el contenido del label
+            if (isRequired)
+            {
+                output.Content.SetHtmlContent($"{labelText} <span style='color: red;'>*</span>");
+            }
+            else
+            {
+                output.Content.SetHtmlContent(labelText);
+            }
+        }
+    }
+}
